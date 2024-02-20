6991fa159b18d530766f80146b9435b2086ec002
Abdullah Morsy <abdallamorsy@hotmail.com>
2024-02-20 17:43:04 +0200

    add files

---
+++ Print/print_1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+	std::cout << "I love programming\n" << std::endl;
+	std::cout << "I promise to be the best developer Ever\n" << std::endl;
+	std::cout << "I know it will take some time to practice, but I will achieve my goal!\n" << std::endl;
+	std::cout << "Best Regards!\nAbdUllah Morsy---Godzilla" << std::endl;
+
+	return 0;
+}
